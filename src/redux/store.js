18dfba0b80d0a19a05180c5362f39fef2531b0d5
10Artur <artyr11012002@gmail.com>
2023-10-21 11:13:56 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactReducer } from './contactsSlice';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsReducer } from './contacts/contactsSlice';
+import { filterReducer } from './contacts/filtersSlice';
+import { authReducer } from './auth/authSlice';
+
+// Постійне поле маркера від фрагмента авторизації до локального сховища
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
 
 export const store = configureStore({
     reducer: {
-        contactData: contactReducer,
+        auth: persistReducer(authPersistConfig, authReducer),
+        contacts: contactsReducer,
+        filter: filterReducer,
     },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+    devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
