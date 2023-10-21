d4b4ff9790544ae24a4df8226f94c8b2bc8d9b5b
10Artur <artyr11012002@gmail.com>
2023-10-21 11:14:30 +0300

    create redux/contacts

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,51 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchContact = createAsyncThunk(
+    'contacts/fetch',
+    async(_, thunkAPI) => {
+        try {
+            const response = await axios.get('/contacts');
+            // console.log(response.data);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const addContact = createAsyncThunk(
+    'contacts/add',
+    async({ name, number }, thunkAPI) => {
+        try {
+            const response = await axios.post('/contacts', { name, number });
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const deleteContact = createAsyncThunk(
+    'contacts/delete',
+    async(contactId, thunkAPI) => {
+        try {
+            const response = await axios.delete(`/contacts/${contactId}`);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const contactsUpdate = createAsyncThunk(
+    'contacts/update',
+    async(contactId, thunkAPI) => {
+        try {
+            const response = await axios.patch(`/contacts/${contactId}`);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
---
+++ src/redux/contacts/selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectLoading = state => state.contacts.isLoading;
+
+export const selectContacts = state => state.contacts.items;
+
+export const selectFilter = state => state.filter;
+
+export const selectVisibleContacts = createSelector(
+    [selectContacts, selectFilter],
+    (contacts, filter) => {
+        console.log('Calculating visible tasks');
+        return contacts.filter(({ name }) =>
+            name.toLowerCase().trim().includes(filter.toLowerCase())
+        );
+    }
+);
